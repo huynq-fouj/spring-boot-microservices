779e9e87121c6cc68cd7a9440518b762b530ebce
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2024-09-09 09:40:38 +0700

    🪄Check public endpoint

--- api-gateway/src/main/java/com/kachina/api_gateway/config/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/kachina/api_gateway/config/AuthenticationFilter.java
@@ -2,15 +2,18 @@ package com.kachina.api_gateway.config;
 
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.server.ServerWebExchange;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -30,6 +33,14 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     private final String TOKEN_PREFIX = "Bearer ";
     private final IdentityService identityService;
     private final ObjectMapper objectMapper;
+    private final PathMatcher pathMatcher;
+
+    private final String[] PUBLIC_ENDPOINT = {
+        "/identity/auth/**"
+    };
+
+    @Value("${app.api-prefix}")
+    private String apiPrefix;
 
     @Override
     public int getOrder() {
@@ -40,6 +51,8 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("Enter authentication filter...");
+        //Check public endpoint
+        if(isPublicEndPoint(exchange.getRequest())) return chain.filter(exchange);
         // Get token form authorization header
         List<String> authHeaders = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
         if(CollectionUtils.isEmpty(authHeaders)) return unauthentication(exchange.getResponse());
@@ -70,6 +83,11 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
         response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         return response.writeWith(Mono.just(response.bufferFactory().wrap(body.getBytes())));
+    }
+
+    private boolean isPublicEndPoint(ServerHttpRequest request) {
+        String requestPath = request.getURI().getPath();
+        return Arrays.stream(PUBLIC_ENDPOINT).anyMatch(s -> pathMatcher.match(apiPrefix + s, requestPath));
     }
     
 }
